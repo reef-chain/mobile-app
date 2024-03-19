4f9684fd05ce8c4ba03483cd0233731c292441b0
Anukul Pandey <anukul.030601@gmail.com>
2024-03-19 17:06:45 +0530

    feat: restart app

--- lib/components/page_layout.dart
+++ lib/components/page_layout.dart
@@ -16,6 +16,7 @@ import 'package:reef_mobile_app/pages/swap_page.dart';
 import 'package:reef_mobile_app/utils/constants.dart';
 import 'package:reef_mobile_app/utils/liquid_edge/liquid_carousel.dart';
 import "package:reef_mobile_app/utils/styles.dart";
+import 'package:restart_app/restart_app.dart';
 
 import 'sign/SignatureContentToggle.dart';
 
@@ -50,8 +51,9 @@ class _BottomNavState extends State<BottomNav> with WidgetsBindingObserver {
       print('APP STATE=$state');
     }
     if(state==AppLifecycleState.resumed) {
-      await ReefAppState.instance.networkCtrl.reconnectProvider();
-      ReefAppState.instance.tokensCtrl.reload(false);
+      Restart.restartApp();
+      // await ReefAppState.instance.networkCtrl.reconnectProvider();
+      // ReefAppState.instance.tokensCtrl.reload(false);
     }
   }
 
