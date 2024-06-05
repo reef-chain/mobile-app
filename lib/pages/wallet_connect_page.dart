69eb5c6aad56637ab82f76d3596d135f1a846f9e
Anukul Pandey <anukul.030601@gmail.com>
2024-06-05 15:25:39 +0530

    fix: list tile issue

--- lib/pages/wallet_connect_page.dart
+++ lib/pages/wallet_connect_page.dart
@@ -17,132 +17,161 @@ class WalletConnectPage extends StatefulWidget {
 
 class _WalletConnectPageState extends State<WalletConnectPage> {
 
-
   @override
   Widget build(BuildContext context) {
     return Column(
-  children: [
-    Container(
-      padding: const EdgeInsets.fromLTRB(12, 24, 12, 32),
-      child: Column(
-        children: [
-          const Gap(16),
-          Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(12, 24, 12, 32),
+          child: Column(
             children: [
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    shadowColor: const Color(0x559d6cff),
-                    elevation: 5,
-                    backgroundColor: Styles.primaryAccentColor,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12, horizontal: 28),
-                  ),
-                  onPressed: () => showQrTypeDataModal(
-                    AppLocalizations.of(context)!.scan_qr_code, context,
-                    expectedType: ReefQrCodeType.walletConnect),
-                  child: Text(
-                    AppLocalizations.of(context)!.create_new_connection,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                      color: Styles.whiteColor
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    ),
-    Expanded(
-      child: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: ValueListenableBuilder<List<SessionData>>(
-                  valueListenable: ReefAppState.instance.walletConnect.sessions,
-                  builder: (context, sessionList, child) {
-                    if (sessionList.isEmpty) {
-                      return Container(
-                        decoration: BoxDecoration(color: Styles.whiteColor,borderRadius: BorderRadius.circular(10.0)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: Icon(Icons.error,color: Styles.errorColor,),
-                            ),
-                            Gap(4.0),
-                            Text(
-                              AppLocalizations.of(context)!.no_active_sessions, 
-                              style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Styles.errorColor)
-                            ),
-                          ],
+              const Gap(16),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
                         ),
-                      );
-                    }
-                    return ListView.separated(
-                      itemCount: sessionList.length,
-                      separatorBuilder: (context, index) => const Gap(10),
-                      itemBuilder: (context, index) {
-                        final session = sessionList[index];
-                        var wcIcon = "https://avatars.githubusercontent.com/u/37784886";
-                        var iconSrc = session.peer.metadata.icons.isNotEmpty
-                                  ? session.peer.metadata.icons[0]
-                                  : wcIcon;
-                        return Container(
-                          decoration: BoxDecoration(
-                            color: Styles.whiteColor,
-                            borderRadius: BorderRadius.circular(10.0)
-                          ),
-                          child: ListTile(
-                            
-                            title: Text(session.peer.metadata.name,style: TextStyle(fontWeight: FontWeight.w600)),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(session.peer.metadata.url),
-                                Text("${AppLocalizations.of(context)!.address}: ${session.namespaces["reef"]?.accounts[0].substring(5).shorten() ?? "???"}"),
-                                Text("Expiry: ${DateFormat('dd/MM/yy HH:mm').format(DateTime.fromMillisecondsSinceEpoch(session.expiry * 1000).toLocal())}",style: TextStyle(fontSize: 12.0),),
-                              ],
-                            ),
-                            leading: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.network(iconSrc,
-                                height: 80),
-                            ),
-                            trailing: IconButton(
-                              icon: const Icon(Icons.delete),
-                              onPressed: () {
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Disconnected ${session.peer.metadata.name} connection")));
-                                ReefAppState.instance.walletConnect.disconnectSession(session.topic);
-                              },
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
+                        shadowColor: const Color(0x559d6cff),
+                        elevation: 5,
+                        backgroundColor: Styles.primaryAccentColor,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 28),
+                      ),
+                      onPressed: () => showQrTypeDataModal(
+                        AppLocalizations.of(context)!.scan_qr_code, context,
+                        expectedType: ReefQrCodeType.walletConnect),
+                      child: Text(
+                        AppLocalizations.of(context)!.create_new_connection,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Styles.whiteColor
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
-      ),
-    ),
-  ],
-);
+        Expanded(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: ValueListenableBuilder<List<SessionData>>(
+                      valueListenable: ReefAppState.instance.walletConnect.sessions,
+                      builder: (context, sessionList, child) {
+                        if (sessionList.isEmpty) {
+                          return Container(
+                            decoration: BoxDecoration(color: Styles.whiteColor,borderRadius: BorderRadius.circular(10.0)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(18.0),
+                                  child: Icon(Icons.error,color: Styles.errorColor,),
+                                ),
+                                Gap(4.0),
+                                Text(
+                                  AppLocalizations.of(context)!.no_active_sessions, 
+                                  style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Styles.errorColor)
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                        return ListView.separated(
+                          itemCount: sessionList.length,
+                          separatorBuilder: (context, index) => const Gap(10),
+                          itemBuilder: (context, index) {
+                            final session = sessionList[index];
+                            var wcIcon = "https://avatars.githubusercontent.com/u/37784886";
+                            var iconSrc = session.peer.metadata.icons.isNotEmpty
+                                      ? session.peer.metadata.icons[0]
+                                      : wcIcon;
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: Styles.whiteColor,
+                                borderRadius: BorderRadius.circular(10.0)
+                              ),
+                              child: CustomSessionTile(
+                                session: session,
+                                iconSrc: iconSrc,
+                                onDelete: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Disconnected ${session.peer.metadata.name} connection")));
+                                  ReefAppState.instance.walletConnect.disconnectSession(session.topic);
+                                },
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
+}
 
+class CustomSessionTile extends StatelessWidget {
+  final SessionData session;
+  final String iconSrc;
+  final VoidCallback onDelete;
+
+  const CustomSessionTile({
+    Key? key,
+    required this.session,
+    required this.iconSrc,
+    required this.onDelete,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(iconSrc, height: 80),
+          ),
+          const Gap(12.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(session.peer.metadata.name, style: TextStyle(fontWeight: FontWeight.w600)),
+                const Gap(4.0),
+                Text(session.peer.metadata.url),
+                const Gap(4.0),
+                Text("${AppLocalizations.of(context)!.address}: ${session.namespaces["reef"]?.accounts[0].substring(5).shorten() ?? "???"}"),
+                const Gap(4.0),
+                Text("Expiry: ${DateFormat('dd/MM/yy HH:mm').format(DateTime.fromMillisecondsSinceEpoch(session.expiry * 1000).toLocal())}", style: TextStyle(fontSize: 12.0)),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: onDelete,
+          ),
+        ],
+      ),
+    );
+  }
 }
