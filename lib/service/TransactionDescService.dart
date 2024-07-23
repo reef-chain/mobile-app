a45c082d43848b66e987ad09a3d60c52d3f73fe3
matjazonline <matjazonline@gmail.com>
2024-07-23 22:43:01 +0200

    try catch error

--- lib/service/TransactionDescService.dart
+++ lib/service/TransactionDescService.dart
@@ -8,35 +8,45 @@ class TransactionDescService {
   static Future<String?> getTransactionDesc(BuildContext context, SignatureRequest? request) async {
     if (request == null) return null;
 
-    var signatureDetails = extractSignatureDetails(request);
+    try {
+      var signatureDetails = extractSignatureDetails(request);
 
-    final method = signatureDetails["Method Name"];
-    final contractAddress = signatureDetails["Contract Address"];
-    final amount = signatureDetails["value"];
-    final recipientAddress = signatureDetails["dest.Id"];
-    const tokenSymbol = "Reef"; // Assuming you have a token symbol
+      final method = signatureDetails["Method Name"];
+      final contractAddress = signatureDetails["Contract Address"];
+      final amount = signatureDetails["value"];
+      final recipientAddress = signatureDetails["dest.Id"];
+      const tokenSymbol = "Reef"; // Assuming you have a token symbol
 
-    String shortAddr(String addr) =>
-        '${addr.substring(0, 2)}...${addr.substring(addr.length - 5)}';
+      String shortAddr(String addr) =>
+          '${addr.substring(0, 2)}...${addr.substring(addr.length - 5)}';
 
-    // Retrieve localized strings
-    final loc = AppLocalizations.of(context)!;
+      // Retrieve localized strings
+      final loc = AppLocalizations.of(context)!;
 
-    if (isMethod(method, 'approve') && isApproveContract(contractAddress)) {
-      return loc.approveTransaction(amount, tokenSymbol, shortAddr(recipientAddress));
-    } else if (isMethod(method, 'swap') && isSwapContract(contractAddress)) {
-      return loc.swapTransaction(amount, tokenSymbol, shortAddr(recipientAddress));
-    } else if (isMethod(method, 'transfer')) {
-      return loc.transferTransaction(amount, shortAddr(recipientAddress));
-    } else if (isMethod(method, 'sendErc20')) {
-      return loc.sendErc20Transaction(amount, tokenSymbol, shortAddr(recipientAddress));
-    } else if (isMethod(method, 'sendNft') && isNftContract(contractAddress)) {
-      final tokenId = request.payload.tokenId; // Assuming tokenId is in payload
-      return loc.sendNftTransaction(tokenId, shortAddr(recipientAddress));
-    } else if (isMethod(method, 'bindAccount')) {
-      return loc.bindAccountTransaction(request.payload.accountDetails); // Assuming account details are in payload
+      if (isMethod(method, 'approve') && isApproveContract(contractAddress)) {
+        return loc.approveTransaction(
+            amount, tokenSymbol, shortAddr(recipientAddress));
+      } else if (isMethod(method, 'swap') && isSwapContract(contractAddress)) {
+        return loc.swapTransaction(
+            amount, tokenSymbol, shortAddr(recipientAddress));
+      } else if (isMethod(method, 'transfer')) {
+        return loc.transferTransaction(amount, shortAddr(recipientAddress));
+      } else if (isMethod(method, 'sendErc20')) {
+        return loc.sendErc20Transaction(
+            amount, tokenSymbol, shortAddr(recipientAddress));
+      } else
+      if (isMethod(method, 'sendNft') && isNftContract(contractAddress)) {
+        final tokenId = request.payload
+            .tokenId; // Assuming tokenId is in payload
+        return loc.sendNftTransaction(tokenId, shortAddr(recipientAddress));
+      } else if (isMethod(method, 'bindAccount')) {
+        return loc.bindAccountTransaction(request.payload
+            .accountDetails); // Assuming account details are in payload
+      }
+    }catch (e) {
+      // TODO error here
+      print('ERROR getTransactionDesc=$e');
     }
-
     return null;
   }
 
