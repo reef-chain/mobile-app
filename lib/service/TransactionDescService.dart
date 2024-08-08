5a8b168494d923133f14bd8bd61d7e2be7352d3b
Anukul Pandey <anukul.030601@gmail.com>
2024-08-09 00:35:50 +0530

    removed unused code

--- lib/service/TransactionDescService.dart
+++ lib/service/TransactionDescService.dart
@@ -1,71 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:reef_mobile_app/model/signing/signature_request.dart';
-import 'package:reef_mobile_app/utils/extract_signature_details.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 
 class TransactionDescService {
   static Future<String?> getTransactionDesc(BuildContext context, SignatureRequest? request) async {
-    if (request == null) return null;
+    // if (request == null) return null;
 
-    try {
-      var signatureDetails = extractSignatureDetails(request);
+    // try {
+    //   var signatureDetails = extractSignatureDetails(request);
+      
+    //   final methodName = signatureDetails["Method Name"];
 
-      final method = signatureDetails["Method Name"];
-      final contractAddress = signatureDetails["Contract Address"];
-      final amount = signatureDetails["value"];
-      final recipientAddress = signatureDetails["dest.Id"];
-      const tokenSymbol = "Reef"; // Assuming you have a token symbol
+    //   // if it is evm erc20 transfer 
+    //   if(methodName=="transfer"){
+    //     final contractAddress = signatureDetails["Contract Address"]; 
 
-      String shortAddr(String addr) =>
-          '${addr.substring(0, 2)}...${addr.substring(addr.length - 5)}';
+    //     final toAddress = signatureDetails["to"];
+    //     final amount = signatureDetails["amount"];
+    //     return "transferring ${amount} to ${toAddress}";
+    //   }
+    //   // if it is native reef transfer
+    //   else if(methodName=="balances.transfer"){
+    //     final dest = signatureDetails["dest.Id"];
+    //     final value = signatureDetails["value"];
+    //     return "transferring ${value} to ${dest.toString().shorten()}";
+    //   }
 
-      // Retrieve localized strings
-      final loc = AppLocalizations.of(context)!;
-
-      if (isMethod(method, 'approve') && isApproveContract(contractAddress)) {
-        return loc.approveTransaction(
-            amount, tokenSymbol, shortAddr(recipientAddress));
-      } else if (isMethod(method, 'swap') && isSwapContract(contractAddress)) {
-        return loc.swapTransaction(
-            amount, tokenSymbol, shortAddr(recipientAddress));
-      } else if (isMethod(method, 'transfer')) {
-        return loc.transferTransaction(amount, shortAddr(recipientAddress));
-      } else if (isMethod(method, 'sendErc20')) {
-        return loc.sendErc20Transaction(
-            amount, tokenSymbol, shortAddr(recipientAddress));
-      } else
-      if (isMethod(method, 'sendNft') && isNftContract(contractAddress)) {
-        final tokenId = request.payload
-            .tokenId; // Assuming tokenId is in payload
-        return loc.sendNftTransaction(tokenId, shortAddr(recipientAddress));
-      } else if (isMethod(method, 'bindAccount')) {
-        return loc.bindAccountTransaction(request.payload
-            .accountDetails); // Assuming account details are in payload
-      }
-    }catch (e) {
-      // TODO error here
-      print('ERROR getTransactionDesc=$e');
-    }
-    return null;
-  }
-
-  static bool isMethod(String? method, String constMethod) {
-    return method?.contains(constMethod) ?? false;
-  }
-
-  static bool isApproveContract(String? contractAddress) {
-    // Implement your logic to verify the contract address for approval
-    return true;
-  }
-
-  static bool isSwapContract(String? contractAddress) {
-    // Implement your logic to verify the contract address for swap
-    return true;
-  }
-
-  static bool isNftContract(String? contractAddress) {
-    // Implement your logic to verify the contract address for NFT
-    return true;
+    //   print("getTransactionDesc ${signatureDetails}");
+    // }catch(e){
+    //   print("ERROR getTransactionDesc ${e}");
+    // }
+    return "";
   }
 }
