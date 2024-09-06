f2e22955a4a42941c4f4c08e99644427907f07ed
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2024-09-06 14:42:35 +0530

    feat: selected account preloader (#153)
    * min amount preloader
    
    * feat: reset ui
    
    ---------
    
    Co-authored-by: matjazonline <matjazonline@gmail.com>

--- lib/pages/stealthex_buy_page.dart
+++ lib/pages/stealthex_buy_page.dart
@@ -36,12 +36,12 @@ class _StealthexBuyPageState extends State<StealthexBuyPage> {
   bool isCalculating = false;
   String txHash = "";
   double minAmount = 0;
+  bool isMinAmountLoading = true;
   TextEditingController searchController = TextEditingController();
 
   FocusNode _focusNode = FocusNode();
 
   ValueNotifier<List<dynamic>> _filteredCurrenciesNotifier = ValueNotifier([]);
-
 
   @override
   void initState() {
@@ -52,19 +52,20 @@ class _StealthexBuyPageState extends State<StealthexBuyPage> {
     // Fetch the list of currencies
     setState(() {
       currencies = ReefAppState.instance.model.stealthexModel.currencies;
+
        _filteredCurrenciesNotifier.value = currencies;
     })
     });
   }
 
-void _filterCurrencies() {
-  String searchText = searchController.text.toLowerCase();
-  _filteredCurrenciesNotifier.value = currencies
-      .where((currency) =>
-          currency['name'].toLowerCase().contains(searchText) ||
-          currency['symbol'].toLowerCase().contains(searchText))
-      .toList();
-}
+  void _filterCurrencies() {
+    String searchText = searchController.text.toLowerCase();
+    _filteredCurrenciesNotifier.value = currencies
+        .where((currency) =>
+            currency['name'].toLowerCase().contains(searchText) ||
+            currency['symbol'].toLowerCase().contains(searchText))
+        .toList();
+  }
 
   void _onFocusChange() {
     setState(() {
@@ -105,128 +106,130 @@ void _filterCurrencies() {
     searchController.dispose();
   }
 
-void openDropdown() async {
-  if (currencies.length > 0) {
-    showModal(context, headText: "Purchase using", child: Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.transparent,
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: searchController,
-              onChanged: (val){
-                _filterCurrencies();
-              },
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 2),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                hintText: "Search tokens",
-                hintStyle: TextStyle(color: Styles.textLightColor),
-              ),
-            ),
-          ),
-        ValueListenableBuilder<List<dynamic>>(
-  valueListenable: _filteredCurrenciesNotifier,
-  builder: (context, filteredCurrencies, child) {
-              return SizedBox(
-              height: 300,
-              child: ListView.builder(
-                  itemCount: filteredCurrencies.length,
-        itemBuilder: (context, index) {
-          var currency = filteredCurrencies[index];
-                  return Container(
-                     decoration: BoxDecoration(
-    border: Border.all(
-      color: (currency['symbol'].toString().toUpperCase() == 'ETH' || 
-              currency['symbol'].toString().toUpperCase() == 'BNB')
-          ? Styles.primaryAccentColor
-          : Colors.transparent,
-      width: 2,
-    ),
-    borderRadius: BorderRadius.circular(10),
-  ),
-                    child: ListTile(
-                      iconColor: Styles.whiteColor,
-                      leading: Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Styles.boxBackgroundColor,
-                          border: Border.all(
-                            color: const Color.fromARGB(193, 255, 255, 255),
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.network(
-                            currency['icon_url'],
-                            width: 24,
-                            height: 24,
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        currency['name'],
-                        style: TextStyle(color: Styles.textColor),
-                      ),
-                      subtitle: Text(
-                        currency['symbol'].toString().toUpperCase(),
-                        style: TextStyle(color: Styles.textLightColor),
-                      ),
-                      onTap: () async {
-                        setState(() {
-                          selectedCurrency = currency;
-                          currencyController.text = currency['symbol'];
-                        });
-                                
-                        var res = await ReefAppState
-                            .instance.stealthexCtrl
-                            .getExchangeRange(currency!["symbol"], currency!["network"]);
-                                
-                        setState(() {
-                          minAmount = res["min_amount"];
-                          Navigator.pop(context);
-                        });
-                      },
-                    ),
-                  );
-                },
-              ),
-            );
-            }
-          ),
-        ],
-      ),
-    ));
-  } else {
-    await ReefAppState.instance.stealthexCtrl.cacheCurrencies();
-
-    var res = ReefAppState.instance.model.stealthexModel.currencies;
-
+  void resetUi(currency) async {
     setState(() {
-      currencies = res;
-     _filteredCurrenciesNotifier.value = currencies;
+      isMinAmountLoading = true;
     });
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("Fetching currencies, Please try again!"),
-        duration: Duration(seconds: 2),
-      ),
-    );
+    var res = await ReefAppState.instance.stealthexCtrl
+        .getExchangeRange(currency!["symbol"], currency!["network"]);
+    setState(() {
+      minAmount = res["min_amount"];
+      isMinAmountLoading=false;
+      amountController.text="";
+      inputAmount=0;
+      estimatedReef=0;
+    });
   }
-}
+
+  void openDropdown() async {
+    if (currencies.length > 0) {
+      showModal(context,
+          headText: "Purchase using",
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.transparent,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: searchController,
+                    onChanged: (val) {
+                      _filterCurrencies();
+                    },
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      hintText: "Search tokens",
+                      hintStyle: TextStyle(color: Styles.textLightColor),
+                    ),
+                  ),
+                ),
+                ValueListenableBuilder<List<dynamic>>(
+                    valueListenable: _filteredCurrenciesNotifier,
+                    builder: (context, filteredCurrencies, child) {
+                      return SizedBox(
+                        height: 300,
+                        child: ListView.builder(
+                          itemCount: filteredCurrencies.length,
+                          itemBuilder: (context, index) {
+                            var currency = filteredCurrencies[index];
+                            return ListTile(
+                              iconColor: Styles.whiteColor,
+                              leading: Container(
+                                width: 40,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: Styles.boxBackgroundColor,
+                                  border: Border.all(
+                                    color: const Color.fromARGB(
+                                        193, 255, 255, 255),
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SvgPicture.network(
+                                    currency['icon_url'],
+                                    width: 24,
+                                    height: 24,
+                                  ),
+                                ),
+                              ),
+                              title: Text(
+                                currency['name'],
+                                style: TextStyle(color: Styles.textColor),
+                              ),
+                              subtitle: Text(
+                                currency['symbol'].toString().toUpperCase(),
+                                style: TextStyle(color: Styles.textLightColor),
+                              ),
+                              onTap: () async {
+                                setState(() {
+                                  selectedCurrency = currency;
+                                  currencyController.text = currency['symbol'];
+                                });
+
+                                Navigator.pop(context);
+
+                                // preloader here
+                                resetUi(currency);
+                              },
+                            );
+                          },
+                        ),
+                      );
+                    }),
+              ],
+            ),
+          ));
+    } else {
+      await ReefAppState.instance.stealthexCtrl.cacheCurrencies();
+
+      var res = ReefAppState.instance.model.stealthexModel.currencies;
+
+      setState(() {
+        currencies = res;
+        _filteredCurrenciesNotifier.value = currencies;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Fetching currencies, Please try again!"),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
 
   ConnectWrapperButton getPurchaseBtn() {
     return ConnectWrapperButton(
@@ -586,7 +589,7 @@ void openDropdown() async {
                             inputAmount = double.tryParse(val)??0.0;
                             isCalculateBtn = true;
                           });
-                          
+
                         },
                         focusNode: _focusNode,
                         inputFormatters: [
@@ -605,6 +608,39 @@ void openDropdown() async {
                       ),
                     ),
                     Gap(16.0),
+                    if(minAmount==0 && selectedCurrency!=null)
+                    Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Currency Threshold",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Styles.textLightColor,
+                                  ),
+                                ),
+                                  JumpingDots(
+                                    animationDuration:
+                                        const Duration(milliseconds: 200),
+                                    verticalOffset: 5,
+                                    radius: 5,
+                                    color: Styles.purpleColor,
+                                    innerPadding: 2,
+                                  ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     if (estimatedReef > 0 || minAmount > 0)
                       Container(
                         padding:
@@ -625,12 +661,34 @@ void openDropdown() async {
                                     color: Styles.textLightColor,
                                   ),
                                 ),
+                                if(!isMinAmountLoading)
                                 Text(
                                   "${minAmount.toDouble().toStringAsFixed(4)} ${selectedCurrency!["symbol"].toString().toUpperCase()}s",
                                   style: TextStyle(
                                     fontWeight: FontWeight.bold,
                                     color: Styles.primaryAccentColor,
                                   ),
+                                ),
+                                if(isMinAmountLoading)
+                                Row(
+                                  children: [
+                                    JumpingDots(
+                                        animationDuration:
+                                            const Duration(milliseconds: 200),
+                                        verticalOffset: 5,
+                                        radius: 5,
+                                        color: Styles.purpleColor,
+                                        innerPadding: 2,
+                                      ),
+                                      Gap(2.0),
+                                       Text(
+                                  "${selectedCurrency!["symbol"].toString().toUpperCase()}s",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Styles.primaryAccentColor,
+                                  ),
+                                ),
+                                  ],
                                 ),
                               ],
                             ),
